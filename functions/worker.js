2b2f8581e040fa80771a1ab66cd913296191c758
yassinmejd1 <69733978+yassinmejd1@users.noreply.github.com>
2025-07-26 17:23:28 +0100

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -1,5 +1,9 @@
 export default {
   async fetch(request, env, ctx) {
-    return env.ASSETS.fetch(request);
+const url = new URL(request.url);
+
+    url.hostname = "mgfcv.webghost.bid"; // your true origin, not mapped to this Worker
+    const newRequest = new Request(url.toString(), request);
+    return env.ASSETS.fetch(newRequest);
   },
 };
